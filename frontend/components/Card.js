205a4fbe1aa839fdea163a102e47c49666ac7f84
Glenn de Haan <glenn@dehaan.cloud>
2019-11-10 09:27:15 +0100

    Replaced grid layout with sections. And implemented virtual list

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -3,65 +3,9 @@ import {connect} from "unistore/preact";
 
 import {actions} from "../modules/store";
 import stringUtils from '../utils/strings';
-import cache from '../utils/cache';
 import Link from "./Link";
 
 class Card extends Component {
-    /**
-     * Constructor
-     */
-    constructor() {
-        super();
-
-        this.state = {
-            image: {
-                blob: null,
-                originalSource: null,
-                source: null,
-                extension: null
-            }
-        };
-
-        this.observer = null;
-        this.card = null;
-    }
-
-    /**
-     * Runs then component mounts
-     */
-    componentDidMount() {
-        this.observer = new IntersectionObserver((entries) => this.intersectionCallback(entries));
-        this.observer.observe(this.card);
-    }
-
-    /**
-     * Checks for intersection events
-     *
-     * @param entries
-     */
-    intersectionCallback(entries) {
-        entries.forEach((entry) => {
-            const visiblePct = entry.intersectionRatio;
-
-            if(visiblePct > 0) {
-                cache.get(`/images/patterns/small/${this.props.slug}_1.jpg`, (image) => {
-                    this.setState({
-                        image
-                    });
-                });
-            } else {
-                this.setState({
-                    image: {
-                        blob: null,
-                        originalSource: null,
-                        source: null,
-                        extension: null
-                    }
-                });
-            }
-        });
-    }
-
     /**
      * Share an oilpattern
      */
@@ -95,42 +39,41 @@ class Card extends Component {
      */
     render() {
         return (
-            <div className="card-wide mdl-card mdl-shadow--2dp" ref={(c) => this.card = c}>
-                <div className="mdl-card__title" style={{background: `${this.state.image.source !== null ? `url(${this.state.image.source})` : 'none'} center / cover`}}>
-                    <h2 className="mdl-card__title-text">
-                        {this.props.title}
-                    </h2>
-                </div>
-                <div className="mdl-card__supporting-text">
-                    {stringUtils.truncate(stringUtils.stripHtml(this.props.children))}
-                </div>
-                {this.props.buttons &&
-                    <div className="mdl-card__actions mdl-card--border">
-                        {this.props.buttons.map((button, key) => {
-                            if(button === 'View') {
-                                return (
-                                    <Link key={key} href={`/pattern/${this.props.slug}`} className="mdl-button mdl-button--colored">
-                                        {button}
-                                    </Link>
-                                )
-                            } else {
-                                return (
-                                    <a key={key} href={this.props.online && `/docs/patterns/${this.props.slug}.pdf`} download={`${this.props.slug}.pdf`} disabled={!this.props.online} className="mdl-button mdl-button--colored">
-                                        {button}
-                                    </a>
-                                )
-                            }
-                        })}
+            <section className="mdl-grid mdl-grid--no-spacing mdl-shadow--2dp row">
+                <header className="mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-tablet mdl-cell--4-col-phone" style={{background: `url(/images/patterns/small/${this.props.slug}_1.jpg) center / cover`}}/>
+                <div className="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
+                    <div className="mdl-card__supporting-text">
+                        <h4>{this.props.title}</h4>
+                        {stringUtils.truncate(stringUtils.stripHtml(this.props.children))}
                     </div>
-                }
-                {this.props.topIcon &&
-                    <div className="mdl-card__menu">
-                        <button className="mdl-button mdl-button--icon" onClick={() => this.share()}>
-                            <i className="material-icons">{this.props.topIcon}</i>
-                        </button>
-                    </div>
-                }
-            </div>
+                    {this.props.buttons &&
+                        <div className="mdl-card__actions mdl-card--border">
+                            {this.props.buttons.map((button, key) => {
+                                if(button === 'View') {
+                                    return (
+                                        <Link key={key} href={`/pattern/${this.props.slug}`} className="mdl-button mdl-button--colored">
+                                            {button}
+                                        </Link>
+                                    )
+                                } else {
+                                    return (
+                                        <a key={key} href={this.props.online && `/docs/patterns/${this.props.slug}.pdf`} download={`${this.props.slug}.pdf`} disabled={!this.props.online} className="mdl-button mdl-button--colored">
+                                            {button}
+                                        </a>
+                                    )
+                                }
+                            })}
+                        </div>
+                    }
+                    {this.props.topIcon &&
+                        <div className="mdl-card__menu">
+                            <button className="mdl-button mdl-button--icon" onClick={() => this.share()}>
+                                <i className="material-icons">{this.props.topIcon}</i>
+                            </button>
+                        </div>
+                    }
+                </div>
+            </section>
         );
     }
 }
--- frontend/pages/Home.js
+++ frontend/pages/Home.js
@@ -1,5 +1,6 @@
 import {h, Component} from 'preact';
 import {connect} from "unistore/preact";
+import VirtualList from 'preact-virtual-list';
 
 import config from '../config';
 import {pageIntro} from '../utils/transitions';
@@ -28,37 +29,38 @@ class Home extends Component {
     }
 
     /**
+     * Renders one pattern card
+     *
+     * @param pattern
+     * @return {*}
+     */
+    renderRow(pattern) {
+        return (
+            <Card buttons={["View", "Download"]} topIcon="share" title={pattern.title} slug={pattern.id}>
+                {pattern.description}
+            </Card>
+        );
+    }
+
+    /**
      * Preact render function
      *
      * @returns {*}
      */
     render() {
+        let patterns = [];
+        if(this.props.search === '') {
+            patterns = config.patterns;
+        } else {
+            patterns = config.patterns.filter((item) => {
+                return item.title.toLowerCase().includes(this.props.search.toLowerCase());
+            });
+        }
+
         return (
             <main className="mdl-layout__content" ref={(c) => this.domElements.mainContainer = c}>
                 <div className="page-content">
-                    <div className="mdl-grid">
-                        {config.patterns.map((pattern, key) => {
-                            if(this.props.search === '') {
-                                return (
-                                    <div key={key} className="mdl-cell mdl-cell--4-col">
-                                        <Card buttons={["View", "Download"]} topIcon="share" title={pattern.title} slug={pattern.id}>
-                                            {pattern.description}
-                                        </Card>
-                                    </div>
-                                )
-                            } else {
-                                if(pattern.title.toLowerCase().includes(this.props.search.toLowerCase())) {
-                                    return (
-                                        <div key={key} className="mdl-cell mdl-cell--4-col">
-                                            <Card buttons={["View", "Download"]} topIcon="share" title={pattern.title} slug={pattern.id}>
-                                                {pattern.description}
-                                            </Card>
-                                        </div>
-                                    )
-                                }
-                            }
-                        })}
-                    </div>
+                    <VirtualList class="list" data={patterns} rowHeight={280} renderRow={this.renderRow} overscanCount={10}/>
                 </div>
             </main>
         );
